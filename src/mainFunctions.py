25b6e1b813902c4af196b46e087c89f4cde8296d
brandon <pokemonbrandon1234@gmail.com>
2017-08-15 08:12:36 +0100

    Updated the code with youtube_dl downloading and functions for show search and episode searching.

--- src/mainFunctions.py
+++ src/mainFunctions.py
@@ -7,6 +7,7 @@ import platform
 import sys
 
 import requests
+import youtube_dl
 from crunchyroll.apis.errors import ApiLoginFailure
 from crunchyroll.apis.meta import MetaApi
 from crunchyroll.apis.meta import ScraperApi
@@ -23,11 +24,9 @@ crunchyroll_scraper_api = ScraperApi(connector=requests)
 def login_to_crunchyroll(crunchyroll_username, crunchyroll_password):
     try:
         crunchyroll_meta_api.login(username=crunchyroll_username, password=crunchyroll_password)
-
     except ApiLoginFailure:
         print("Your login failed, please try again.")
         quit()
-
     else:
         print("Your login succeeded.")
 
@@ -47,7 +46,6 @@ def show_search(user_show_search_string):
     if len(user_show_search_output) == 0:
         print("The show you are looking for is unavailable, please try again.")
         main()
-
     print("Here are your search results: \n")
     for show_number in range(len(user_show_search_output)):
         print("[{0}]: ".format(show_number + 1) + user_show_search_output[show_number].name)
@@ -59,16 +57,50 @@ def show_search(user_show_search_string):
     except:
         print("Number entered or their is an error, please try again.")
         main()
-
     confirmation = input("Are you sure that {0} is the anime you want to watch?: ".format(
         user_show_search_output[user_show_result - 1].name)).lower()
-
     if confirmation == "yes":
         return user_show_search_output[user_show_result - 1]
 
 
+def episode_choice(user_show_choice_result, simulate_boolean):
+    print("These are the list of episodes available to watch. \n")
+    episodes_from_user_show = crunchyroll_meta_api.list_media(user_show_choice_result)
+    amount_of_episodes = len(episodes_from_user_show)
+    for x in episodes_from_user_show:
+        print("[{0}] Episode {1}: {2}".format(amount_of_episodes, x.episode_number,
+                                              x.name))  # Prints the available list of episodes.
+        amount_of_episodes -= 1
+    episode_id_input = input("Input the ID of the episode(s) that you would like to watch: ")
+    ydl_opts = {
+        "simulate": simulate_boolean,
+        "subtitlesformat": "ass",
+        "subtitleslangs": ["enUS"],
+        "writesubtitles": True,
+        "call_home": False,
+        "outtmpl": "%(season)s - Episode %(episode_number)s: %(episode)s.%(ext)s",
+    }
+
+    if episode_id_input == "":
+        print("Downloading all episodes.")
+    else:
+        if "-" or "," in episode_id_input:
+            print("Downloading multiple episodes.")
+        if "-" or "," not in episode_id_input:
+            selected_episode = episodes_from_user_show[len(episodes_from_user_show) - int(episode_id_input)]
+            print("Downloading episode {0}".format(selected_episode.episode_number))
+            episode_premium_only = not (bool(selected_episode.free_available))
+            episode_media_id = selected_episode.media_id
+            episode_url = selected_episode.url
+        ydl_opts["playlist_items"] = episode_id_input
+    the_url_for_the_show = user_show_choice_result.url
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([the_url_for_the_show])
+
+
 def main():
     # Booleans
+    simulate_download = False
     login = False
     queue_argument = False
 
@@ -95,6 +127,7 @@ def main():
 
     user_show_search = input("Search for a show: ")
     user_show_choice = show_search(user_show_search)
+    episode_choice(user_show_choice, simulate_download)
 
 
 main()
